0340f93980b52b9879e0e30de6d2c48bcad4afcb
Clari Ursini <clari_ursini@hotmail.com>
2023-01-28 23:50:07 -0300

    adedd skip before action and dissble nav methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,21 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :onboarding, :slide1, :slide2, :slide3 ]
+
+  def onboarding
+    @disable_nav = true
+  end
+
+  def slide1
+    @disable_nav = true
+  end
+
+  def slide2
+    @disable_nav = true
+  end
+
+  def slide3
+    @disable_nav = true
+  end
 
   def home
   end
