92fb81d1531a55d3a4c6d4cdaf8546d8bd8c77b0
Clari Ursini <clari_ursini@hotmail.com>
2023-01-28 23:53:29 -0300

    added routes for home and slides

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#onboarding", as: 'onboarding'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'home', to: 'pages#home', as: 'home'
+  get 'slide1', to: 'pages#slide1', as: 'slide1'
+  get 'slide2', to: 'pages#slide2', as: 'slide2'
+  get 'slide3', to: 'pages#slide3', as: 'slide3'
 end
