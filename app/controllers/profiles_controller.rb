e156d88f8f038bb8151cc043f99c275e9978c6bc
Clara Ursini <clari_ursini@hotmail.com>
2023-03-21 09:14:34 -0300

    added profile index view and methods

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,22 @@
 class ProfilesController < ApplicationController
+
+  def profile_index
+    @section_title = "Mi perfil"
+  end
+
+  def show
+  end
+
+  def favourite
+    @section_title = "Favoritos"
+  end
+
+  def notification
+
+  end
+
+  def publication
+    @section_title = "Mis publicaciones"
+  end
+
 end
