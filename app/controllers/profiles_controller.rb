269614a55c743be32c4053c1138318d5036c779d
Clara Ursini <clari_ursini@hotmail.com>
2023-03-22 17:15:01 -0300

    updated profiles mis publicaciones methods

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :set_user, only: %i[show]
+  before_action :sub_links
 
   def profile_index
     @section_title = "Mi perfil"
@@ -14,11 +15,24 @@ class ProfilesController < ApplicationController
   end
 
   def notification
-
+    @section_title = "Notificaciones"
   end
 
-  def publication
+  def publication_caballos
     @section_title = "Mis publicaciones"
+    @horses = Horse.all.where(user_id: current_user)
+  end
+
+  def publication_remates
+    @section_title = "Mis publicaciones"
+    @auctions = Auction.all.where(user_id: current_user)
+  end
+
+  def sub_links
+    @sub_links = [
+      { title: "Caballos", path: profile_publication_caballos_path },
+      { title: "Remates", path: profile_publication_remates_path }
+    ]
   end
 
   private
