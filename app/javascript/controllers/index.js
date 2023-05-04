a5769648d2eeacafdf22fca479691e9334898b9d
Clara Ursini <clari_ursini@hotmail.com>
2023-05-04 13:37:47 -0300

    imported js controller in index

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import StarRatingController from "./star_rating_controller"
 application.register("star-rating", StarRatingController)
+
+import ChatController from "./chat_controller"
+application.register("chat", ChatController)
