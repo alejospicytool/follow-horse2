413b444ccf524824152476bb5fc0dcae50829ad4
Clara Ursini <clari_ursini@hotmail.com>
2023-05-04 13:40:30 -0300

    added messages and conversation models and tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_132113) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_134538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_132113) do
     t.index ["user_id"], name: "index_bids_on_user_id"
   end
 
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "archive", default: false
+  end
+
   create_table "favourites", force: :cascade do |t|
     t.bigint "horse_id", null: false
     t.bigint "user_id", null: false
@@ -106,6 +114,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_132113) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["auction_id"], name: "index_lotes_on_auction_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.bigint "conversation_id", null: false
+    t.bigint "user_id", null: false
+    t.boolean "read", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -153,5 +172,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_132113) do
   add_foreign_key "favourites", "users"
   add_foreign_key "horses", "users"
   add_foreign_key "lotes", "auctions"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "reviews", "users"
 end
