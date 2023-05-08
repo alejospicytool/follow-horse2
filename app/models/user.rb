54d778a0ab707a3ff8e3f05e11ca7e22bda337e0
Clara Ursini <clari_ursini@hotmail.com>
2023-05-08 10:01:59 -0300

    updated user model with message dependant destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :favourites, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :bids, dependent: :destroy
+  has_many :messages, dependent: :destroy
   has_one_attached :photo, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
