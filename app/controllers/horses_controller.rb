9d37c7d553751c1661bdefa5f38b927b3b9216bb
Clara Ursini <clari_ursini@hotmail.com>
2023-05-08 10:03:08 -0300

    updated before action in horses controller

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,5 +1,5 @@
 class HorsesController < ApplicationController
-  before_action :set_horse, only: %i[show edit update delete destroy]
+  before_action :set_horse, only: %i[show edit update destroy]
 
   def index
     @horses = Horse.all.where.not(user_id: current_user.id)
@@ -49,6 +49,62 @@ class HorsesController < ApplicationController
       redirect_to horses_path(@horse)
     else
       render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def filtros
+    @filtro = Filtro.new
+    @horses = Horse.all.where.not(user_id: current_user.id)
+    @jinetes = @horses.map { |horse| horse.rider }.uniq
+    @height = ["0.5", "0.6", "0.7", "0.8", "0.9", "1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "2.0"]
+    @ubicaciones = @horses.map { |horse| User.find(horse.user_id).ciudad }.uniq
+    @section_title = "Caballos - Filtros"
+  end
+
+  def aplicarfiltros
+    jinete = params[:filtro][:jinete]
+    edad = params[:filtro][:edad]
+    altura = params[:filtro][:altura]
+    ubicacion = params[:filtro][:ubicacion]
+    redirect_to horses_results_path(filtro: "OK", jinete: jinete, edad: edad, altura: altura, ubicacion: ubicacion)
+  end
+
+  def resultados
+    @section_title = "Resultados"
+    @filtros = 'true'
+
+    # Resultados
+    if params[:filtro] == "OK"
+      @horses_all = Horse.all.where.not(user_id: current_user.id)
+      @horses = Horse.all.where.not(user_id: current_user.id)
+      # Filtro por jinete
+      if params[:jinete] != ""
+        @horses = @horses_all.where(rider: params[:jinete])
+      end
+      # Filtro por Edad
+      if params[:edad] != "0"
+        if @horses
+          @horses = @horses.where(age: params[:edad])
+        else
+          @horses = @horses_all.where(age: params[:edad])
+        end
+      end
+      # Filtro por Altura
+      if params[:altura] != ""
+        if @horses
+          @horses = @horses.where(height: params[:altura].to_s)
+        else
+          @horses = @horses_all.where(height: params[:altura].to_s)
+        end
+      end
+      # Filtro por Ubicacion
+      if params[:ubicacion] != ""
+        if @horses
+          @horses = @horses.where(user_id: User.where(ciudad: params[:ubicacion]).pluck(:id))
+        else
+          @horses = @horses_all.where(user_id: User.where(ciudad: params[:ubicacion]).pluck(:id))
+        end
+      end
     end
   end
 
