135f0248f0801040bf57495c305ef4aca23678e3
Clara Ursini <clari_ursini@hotmail.com>
2023-08-05 13:42:31 -0300

    updated contact controller to open  modal

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,10 +1,11 @@
 class ContactController < ApplicationController
   def index
+    @section_title = "Ayuda"
   end
 
   def send_message
     user_message = params[:message]
     ContactMailer.send_contact_message(user_message).deliver_now
-    redirect_to home_path, notice: "Your message has been sent. Thank you!"
+    redirect_to contact_path(mensaje_enviado: 'ok')
   end
 end
