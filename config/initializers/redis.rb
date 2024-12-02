c45d69fec8e6ef5b99c0decb15099aedd902882b
ezequiel <ezequielfernandez94@hotmail.com>
2024-12-02 07:20:12 -0300

    Changing redis credentials

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 $redis = Redis.new
-url = ENV["REDISCLOUD_URL"]
+## url = "redis://default:wFob4MQ1gr4ntgZMzHx4eThGaEjsyBk4@redis-17689.c270.us-east-1-3.ec2.cloud.redislabs.com:17689"
+url = "redis://default:Ge42H55fUrH8hfukHFXTDebvu2RcLdcV@redis-10527.c84.us-east-1-2.ec2.cloud.redislabs.com:10527"
 
 if url
 
